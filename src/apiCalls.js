2a37eebfa6965d38362c9ff2e7de8350d3a7cff9
Kevin King <kevinturingking@gmail.com>
2022-04-02 13:36:11 -0600

    create API function to get all data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,28 @@
 // Your fetch requests will live here!
+// fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users').then(response => response.json()).then(data => console.log(data));
+//
+// //Ingredients Data
+// fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients').then(response => response.json()).then(data => console.log(data));
+//
+// //Recipe Data
+// fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes').then(response => response.json()).then(data => console.log(data));
+
+const fetchData = (extension) => {
+  return fetch(`https://what-s-cookin-starter-kit.herokuapp.com/api/v1/${extension}`)
+    .then(response => response.json())
+    .catch(err => console.log(err));
+};
+
+// function fetchAllData() {
+//   Promise.all([fetchData('users'), fetchData('ingredients'), fetchData('recipes')])
+//     .then(data => {
+// 			console.log(data)
+//       // userData = data[0]
+//       // initializeUserData(data[0].userData, data[1].ingredientData, data[2].recipeData);
+//       // getCurrentUser(1);
+//   });
+// };
+// fetchAllData();
 
 
-console.log('I will be a fetch request!')
+export {fetchData}
